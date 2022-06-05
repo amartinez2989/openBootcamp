77b3b3dbb30acb20a264ffed5e7c95b7dc94cc8d
Andrés Martinez <amartinez@gamil.com>
2022-06-05 20:29:19 -0300

    Ejercicio primera parte

--- Ejercicio1-tema3/src/Main.java
+++ Ejercicio1-tema3/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 2;
+        int b = 4;
+        suma(a,b);
+    }
+
+
+    public static void suma(int a, int b){
+        int resultado = a + b;
+        System.out.println(resultado);
+
     }
 }
+
