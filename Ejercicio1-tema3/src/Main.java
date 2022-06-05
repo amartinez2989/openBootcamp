4ed249dcf73789a2c99dc5e884691715087397a4
Andrés Martinez <amartinez@gamil.com>
2022-06-05 20:44:34 -0300

    Ejercicio segunda parte - Creacion de objeto miCoche y funcion agregar puertas

--- Ejercicio1-tema3/src/Main.java
+++ Ejercicio1-tema3/src/Main.java
@@ -3,13 +3,24 @@ public class Main {
         int a = 2;
         int b = 4;
         suma(a,b);
+
+        // creacion del objeto auto
+        coche miCoche = new coche();
+        miCoche.incrementarPuerta();
+        System.out.println("Cantitad de puertas del coche: "+miCoche.puerta);
     }
 
 
     public static void suma(int a, int b){
         int resultado = a + b;
-        System.out.println(resultado);
+        System.out.println("Resultado de la suma: " + resultado);
 
     }
 }
+class coche {
+    public int puerta = 4;
 
+    public void incrementarPuerta(){
+        this.puerta ++;
+    }
+}
