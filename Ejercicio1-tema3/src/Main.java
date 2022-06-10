ed5ada76a78140b56adc6c1650ddeaf66a6957b4
Andrés Martinez <amartinez@gamil.com>
2022-06-09 23:36:42 -0300

    Ejercicio 4 parte 2, parte 3, parte 3 y parte 5

--- Ejercicio1-tema3/src/Main.java
+++ Ejercicio1-tema3/src/Main.java
@@ -1,26 +1,58 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 4;
-        suma(a,b);
+    int numeroIf= -9;
+    if (numeroIf==0){
+        System.out.println("numero igual a 0");
+    }else if (numeroIf>0){
+        System.out.println("numero mayor a 0");
+    }else{
+        System.out.println("numero negativo");
+    }
 
-        // creacion del objeto auto
-        coche miCoche = new coche();
-        miCoche.incrementarPuerta();
-        System.out.println("Cantitad de puertas del coche: "+miCoche.puerta);
+        // segunda parte
+        int numeroWhile = 0;
+        while(numeroWhile < 3 ) {
+            numeroWhile = numeroWhile +1;
+            System.out.println("numeroWhile es: "+numeroWhile);
+
+        }
+
+        // tercera parte
+        int numeroDoWile = 2;
+        do {
+            System.out.println("numeroDoWile: "+numeroDoWile);
+            numeroDoWile = numeroWhile +1;
+        } while (numeroDoWile < 3 );
+
+        // cuarta parte
+        int numeroFor = 0;
+        for (int i = 0; i < 3; i++){
+            numeroFor = numeroFor +1;
+            System.out.println("numeroFor: "+numeroFor);
+        }
+
+        // quinta parte
+        String estacion = "verano";
+        switch(estacion) {
+            case "verano":
+                System.out.println("estacion es verano");
+                break;
+            case "invierno":
+                System.out.println("la estacion es inviertno");
+                break;
+            case "otoño":
+                System.out.println("la estacion es otoño");
+                break;
+            case "primavera":
+                System.out.println("la estacion es primavera");
+                break;
+            default:
+                System.out.println("No es una estacion");
+        }
+
     }
 
 
-    public static void suma(int a, int b){
-        int resultado = a + b;
-        System.out.println("Resultado de la suma: " + resultado);
 
-    }
-}
-class coche {
-    public int puerta = 4;
 
-    public void incrementarPuerta(){
-        this.puerta ++;
-    }
 }
